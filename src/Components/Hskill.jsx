5957da335652319eb515d7e02e9ace7f4486c20d
Celvindev <celvin.alfino32@gmail.com>
2024-08-11 11:00:08 +0700

    progress 5

--- src/Components/Hskill.jsx
+++ src/Components/Hskill.jsx
@@ -18,38 +18,38 @@ const Hskill = () => {
         transform: `translateY(${scrollY * -0.6}px)`,
         config: { mass: 1, tension: 280, friction: 80 }
     });
+
     return (
         <>
             <animated.div
                 style={parallax2Effect}
-                className="w-full bg-[#14151A] h-auto mt-3 md:mt-8 p-5 rounded-3xl flex-1 "
+                className="w-full bg-[#14151A] h-auto mt-3 md:mt-8 p-5 rounded-3xl flex-1"
             >
                 <motion.h1
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, delay: 0.4 }}
-                    className="text-xl md:text-2xl  text-center text-white font-poppins font-semibold"
+                    className="text-xl md:text-2xl text-center text-white font-poppins font-semibold"
                 >
                     Work <span className="text-[#07C65D]">Experience</span>
                 </motion.h1>
-                <div className="flex justify-center mt-8 gap-8">
-                    <img src="./icon/ae.ico" className="w-20" />
-                    <img src="./icon/codei.ico" className="w-20" />
-                    <img src="./icon/cshap.ico" className="w-20" />
-                    <img src="./icon/css.ico" className="w-20" />
-                    <img src="./icon/html.ico" className="w-20" />
-                    <img src="./icon/js.ico" className="w-20" />
-                    <img src="./icon/lara.ico" className="w-20" />
-                    <img src="./icon/mysql.ico" className="w-20" />
-                    <img src="./icon/php.ico" className="w-20" />
-                    <img src="./icon/pr.ico" className="w-20" />
-                    <img src="./icon/react.ico" className="w-20" />
-                    <img src="./icon/vue.ico" className="w-20" />
+                <div className="flex justify-center mt-8 overflow-x-scroll space-x-8 scrollbar-hide">
+                    <img src="./icon/ae.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/codei.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/cshap.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/css.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/html.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/js.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/lara.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/mysql.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/php.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/pr.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/react.ico" className="w-20 flex-shrink-0" />
+                    <img src="./icon/vue.ico" className="w-20 flex-shrink-0" />
                 </div>
-
             </animated.div>
         </>
     )
 }
 
-export default Hskill
+export default Hskill;
